ab5d88788a3447b49b790a3351356d38011661e2
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2024-03-27 14:02:49 +0200

    Reformatting some files in phase_2

--- phase_2/exercise_1/madd_main.c
+++ phase_2/exercise_1/madd_main.c
@@ -7,16 +7,21 @@
 #include <CL/cl.h>
 
 #define ORDER 3
-const int CORRECT_ANSWER[ORDER][ORDER] = {{2, 4, 6}, {8, 10, 12}, {14, 16, 18}};
+const int CORRECT_ANSWER[ORDER][ORDER] = {
+    {2,  4,  6 },
+    {8,  10, 12},
+    {14, 16, 18}
+};
 
-void      check_error(cl_int err, const char *desc);
+void check_error(cl_int err, const char *desc);
 
-cl_program compile_program_from_file(const char *path, cl_context ctx,
-                                     cl_int *err);
+cl_program compile_program_from_file(
+    const char *path, cl_context ctx, cl_int *err
+);
 
 bool check_matrices_equal(const int A[ORDER][ORDER], const int B[ORDER][ORDER]);
 
-int  main() {
+int main() {
     cl_int           err;
     cl_uint          num_platforms;
     cl_platform_id  *platforms;
@@ -28,14 +33,26 @@ int  main() {
     cl_event         profiling_evt;
     // result needs to be one bigger than the output since the output doesn't
     // contain a NULL terminator
-    int N = ORDER;
-    int A[ORDER][ORDER] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int B[ORDER][ORDER] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int C[ORDER][ORDER] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    int              N               = ORDER;
+    int              A[ORDER][ORDER] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int B[ORDER][ORDER] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int C[ORDER][ORDER] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
     // Determine platform
     int max_platforms = 1;
-    err = clGetPlatformIDs(max_platforms, NULL, &num_platforms);
+    err               = clGetPlatformIDs(max_platforms, NULL, &num_platforms);
     check_error(err, "getting platform ids");
     printf("Number of platforms detected: %d\n", num_platforms);
 
@@ -76,16 +93,19 @@ int  main() {
     // create device buffers for input matrices and output matrix
     cl_mem d_A, d_B, d_C;
 
-    d_A = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                          sizeof(A), A, &err);
+    d_A = clCreateBuffer(
+        context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(A), A, &err
+    );
     check_error(err, "creating device buffer A");
 
-    d_B = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                          sizeof(B), B, &err);
+    d_B = clCreateBuffer(
+        context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(B), B, &err
+    );
     check_error(err, "creating device buffer B");
 
-    d_C = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR,
-                          sizeof(C), C, &err);
+    d_C = clCreateBuffer(
+        context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(C), C, &err
+    );
     check_error(err, "creating device buffer C");
 
     err = clSetKernelArg(kernel, 0, sizeof(int), &N);
@@ -99,8 +119,9 @@ int  main() {
 
     // Execute the kernel N*N times
     const size_t global_ids[2] = {ORDER, ORDER};
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_ids, NULL, 0,
-                                  NULL, &profiling_evt);
+    err                        = clEnqueueNDRangeKernel(
+        queue, kernel, 2, NULL, global_ids, NULL, 0, NULL, &profiling_evt
+    );
 
     check_error(err, "enqueueueueing task");
 
@@ -111,8 +132,9 @@ int  main() {
     (void)clWaitForEvents(1, &profiling_evt);
 
     // Read kernel output
-    err = clEnqueueReadBuffer(queue, d_C, CL_TRUE, 0, sizeof(C), C, 0, NULL,
-                               NULL);
+    err = clEnqueueReadBuffer(
+        queue, d_C, CL_TRUE, 0, sizeof(C), C, 0, NULL, NULL
+    );
     check_error(err, "reading output");
 
     printf("result matrix:\n");
@@ -138,14 +160,24 @@ int  main() {
     }
 
     // printf profiling information
-    cl_ulong evt_start = 0;
-    cl_ulong evt_end = 0;
+    cl_ulong evt_start    = 0;
+    cl_ulong evt_end      = 0;
     size_t   return_bytes = 0;
 
-    (void)clGetEventProfilingInfo(profiling_evt, CL_PROFILING_COMMAND_QUEUED,
-                                   sizeof(evt_start), &evt_start, &return_bytes);
-    (void)clGetEventProfilingInfo(profiling_evt, CL_PROFILING_COMMAND_END,
-                                   sizeof(evt_end), &evt_end, &return_bytes);
+    (void)clGetEventProfilingInfo(
+        profiling_evt,
+        CL_PROFILING_COMMAND_QUEUED,
+        sizeof(evt_start),
+        &evt_start,
+        &return_bytes
+    );
+    (void)clGetEventProfilingInfo(
+        profiling_evt,
+        CL_PROFILING_COMMAND_END,
+        sizeof(evt_end),
+        &evt_end,
+        &return_bytes
+    );
     double runtime = (double)(evt_end - evt_start);
 
     printf("\nRuntime of the kernel was %.4f microseconds\n", runtime * 1e-3);
@@ -163,12 +195,13 @@ int  main() {
     return 0;
 }
 
-cl_program compile_program_from_file(const char *path, cl_context ctx,
-                                     cl_int *err) {
+cl_program compile_program_from_file(
+    const char *path, cl_context ctx, cl_int *err
+) {
     cl_program program = NULL;
-    FILE      *f = NULL;
-    size_t     sz = 0;
-    char      *str = NULL;
+    FILE      *f       = NULL;
+    size_t     sz      = 0;
+    char      *str     = NULL;
 
     f = fopen(path, "rb");
 
@@ -216,8 +249,9 @@ void check_error(cl_int err, const char *desc) {
     }
 }
 
-bool check_matrices_equal(const int A[ORDER][ORDER],
-                          const int B[ORDER][ORDER]) {
+bool check_matrices_equal(
+    const int A[ORDER][ORDER], const int B[ORDER][ORDER]
+) {
     for (int c = 0; c < ORDER; ++c) {
         for (int r = 0; r < ORDER; ++r) {
             if (A[r][c] != B[r][c]) {
